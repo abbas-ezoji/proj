842367e4d1276483cec92153f795357fc5f551b6
abbas-ezoji <abbas.ezoji@gmail.com>
2019-02-09 20:12:58 +0330

    login 4

--- app1/static/login.js
+++ app1/static/login.js
@@ -13,16 +13,22 @@ $(window).click(function(e) {
 });
 ///////////////////////////////////////////
 function login(){
-    console.log('login')
     var username = $("#usernameTxt").val();
-    var password = $("#password").val();
+    var password = $("#usernameTxt").val();
     if( username =='' || password ==''){
         $('#usernameTxt').css("border","2px solid red");
         $('#usernameTxt').css("box-shadow","0 0 3px red");
         $('#passwordTxt').css("border","2px solid red");
         $('#passwordTxt').css("box-shadow","0 0 3px red");
         alert("لطفاً نام کاربری و رمز را وارد کنید ...");
+    }else{
+            var url = "/api/login/?format=json&username='".concat(username).concat("'&password='").concat(password).concat("'")
+            console.log(url)
+            $.getJSON( url, function( data ) {
+                console.log(data.firstname)
+            })
     }
+
 }
 
 function register(){
